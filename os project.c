f6ad31f787b18a111e8ea7f86943450f6997edd4
NARASIMHA SAI ADDEPALLI <38582295+Narasimha-sai@users.noreply.github.com>
2019-04-19 14:01:25 +0530

    Add files via upload

--- os project.c
+++ os project.c
@@ -2,15 +2,18 @@
 int main()
 {
 int i,n,p[10]={1,2,3,4,5,6,7,8,9,10},min,k=1,btime=0;
-int bursttime[10],temp,j,arivaltime[10],wt[10],turnaroundtime[20],ta=1,sum=0;
+int bursttime[10],temp,j,arivaltime[10],wt[10],turnaroundtime[20],ta=1,sum=0,ct[10];
 float wavg=0,tavg=0,tsum=0,wsum=0;
-printf("Enter the number of processes:\n");
+printf("\nEnter the number of processes:");
 scanf("%d",&n);
-for(i=0;i<n;i++)
-{
-	printf("Arrival time of p%d process :\n",i+1);
-	scanf("%d",&arivaltime[i]);
-	printf("Burst time of P%d process :\n",i+1);
+for(i=0;i<n;i++){
+	printf("\nArrival time of p%d process :",i+1);
+	scanf("%d",&arivaltime[i]);}
+	
+printf("\n****BURSTTIME IS DOUBLE OF ARRIVAL TIME*****\n");
+	for(i=0;i<n;i++){
+	
+	printf("\nBurst time of P%d process :",i+1);
 	bursttime[i]=2*arivaltime[i];
 	printf("%d\n",bursttime[i]);
 }
@@ -34,29 +37,29 @@ for(i=0;i<n;i++)
 }
 wt[0]=0;
 sum=1;
-for(i=1;i<n;i++)
+for(i=1;i<=n;i++)
 {
-	sum=sum+bursttime[i-1];
-	wt[i]=sum-arivaltime[i];
-	wsum=wsum+wt[i];
+	if(arivaltime[i-1]==0)
+	  sum=sum+1;
+sum=sum+bursttime[i-1];
+ct[i-1]=sum;
+turnaroundtime[i]=ct[i-1]-arivaltime[i-1];
+tsum=tsum+turnaroundtime[i];
+wt[i]=turnaroundtime[i]-bursttime[i-1];
+wsum=wsum+wt[i];
 }
+ 
 wavg=(wsum/n);
-for(i=0;i<n;i++)
-{
-	ta=ta+bursttime[i];
-	turnaroundtime[i]=ta-arivaltime[i];
-	tsum=tsum+turnaroundtime[i];
-}
 tavg=(tsum/n);
 printf("\n\t\t\t:-RESULT TABLE :-");
-printf("\n--------------------------------------------------------------------");
-printf("\nPROCESS\t| BURST\t| ARRIVAL\t| WAITING\t| TURN-AROUND-TIME |");
-printf("\n--------------------------------------------------------------------");
+printf("\n-----------------------------------------------------------------------------------------------------");
+printf("\nPROCESS\t| BURST\t| ARRIVAL\t| COMPLETION TIME\t| TURN-AROUND-TIME | \t WAITING TIME   \t|");
+printf("\n-----------------------------------------------------------------------------------------------------");
 for(i=0;i<n;i++)
 {
-	printf("\n| p%d\t|  %d\t|  %d\t\t|  %d\t\t|\t%d\t   |",p[i],bursttime[i],arivaltime[i],wt[i],turnaroundtime[i]);
+	printf("\n| p%d\t|  %d\t|  %d\t\t|\t  %d\t\t|\t%d\t   | \t\t %d\t\t|",p[i],bursttime[i],arivaltime[i],ct[i],turnaroundtime[i+1],wt[i+1]);
 }
-printf("\n--------------------------------------------------------------------\n");
+printf("\n-----------------------------------------------------------------------------------------------------\n");
 for(i=0;i<n;i++)
 {
 printf("\np%d",p[i]);
